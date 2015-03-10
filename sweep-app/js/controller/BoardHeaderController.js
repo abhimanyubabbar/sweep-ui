20a1d932da7bad85c9a72ab7e30c12d7a2d93ebd
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2015-03-10 15:30:16 +0100

    Added the support for the file upload service in UI.

--- sweep-app/js/controller/BoardHeaderController.js
+++ sweep-app/js/controller/BoardHeaderController.js
@@ -2,16 +2,59 @@
 
 angular.module('app')
 
-    .controller('BoardHeaderController', ['$log', '$scope','common', function ($log, $scope, common) {
+    .controller('BoardHeaderController', ['$log', '$scope', 'common', function ($log, $scope, common) {
 
         function _initScope(scope) {
-            
+
             $log.info('Board Header Controller Initialized.');
-            scope.routeTo = function(path){
+            scope.routeTo = function (path) {
                 common.routeTo(path);
             }
         }
-        
+
         _initScope($scope);
-        
+
+    }])
+
+    .directive('fileUploader', ['$log', function ($log) {
+
+        // Upload the file to the system.
+        return{
+            
+            restrict: 'A',
+            link: function (scope, element, attributes) {
+
+                element.bind('change', function (changeEvent) {
+
+                    var reader = new FileReader();
+                    reader.onload = function (loadEvent) {
+
+                        var ymlJson = {
+                            yml: loadEvent.target.result
+                        };
+                        $log.info(JSON.parse(loadEvent.target.result));
+                        
+                        element.val("");
+
+                    };
+                    //Assuming only single file read is allowed.
+                    reader.readAsText(changeEvent.target.files[0]);
+                });
+            }
+        }
+    }])
+    .directive('clickDirective', ['$log', function ($log) {
+
+        return {
+            restrict: 'A',
+            link: function (scope, element, attributes) {
+
+                element.bind('click', function (clickEvent) {
+                    var uploaderElement = angular.element(document.querySelector("#fileUploader"));
+                    uploaderElement.trigger('click');
+                });
+
+            }
+        }
+
     }]);
--- sweep-app/js/statistics/StatisticsController.js
+++ sweep-app/js/statistics/StatisticsController.js
@@ -1,59 +1,73 @@
-
 angular.module('app').
-    controller('StatisticsController', function($scope) {
-    $scope.options = {
-        chart: {
-            type: 'scatterChart',
-            height: 500 ,
-            color: d3.scale.category10().range(),
-            scatter: {
-                onlyCircles: false
-            },
-            showDistX: false,
-            showDistY: false,
-            tooltipContent: function(key) {
-                return '<h3>' + key + '</h3>';
-            },
-            transitionDuration: 350,
-            xAxis: {
-                axisLabel: 'X Axis',
-                tickFormat: function(d){
-                    return d3.format('.02f')(d);
-                }
-            },
-            yAxis: {
-                axisLabel: 'Y Axis',
-                tickFormat: function(d){
-                    return d3.format('.02f')(d);
-                },
-                axisLabelDistance: 30
-            }
-        }
-    };
+    controller('StatisticsController', ['$scope','$log','$interval', function ($scope, $log, $interval) {
 
-    $scope.data = generateData(2,40);
+        /* Random Data Generator (took from nvd3.org) */
+        function generateData(groups, points) {
+            var data = [],
+                shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
+                random = d3.random.normal();
 
-    /* Random Data Generator (took from nvd3.org) */
-    function generateData(groups, points) {
-        var data = [],
-            shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
-            random = d3.random.normal();
-
-        for (var i = 0; i < groups; i++) {
-            data.push({
-                key: 'Group ' + i,
-                values: []
-            });
-
-            for (var j = 0; j < points; j++) {
-                data[i].values.push({
-                    x: random()
-                    , y: random()
-                    , size: Math.random()
-//                        , shape: shapes[j % 6]
+            for (var i = 0; i < groups; i++) {
+                data.push({
+                    key: 'Group ' + i,
+                    values: []
                 });
+
+                for (var j = 0; j < points; j++) {
+                    data[i].values.push({
+                        x: random(), y: random(), size: Math.random()
+//                        , shape: shapes[j % 6]
+                    });
+                }
             }
+            return data;
         }
-        return data;
-    }
-});
+        
+        function _initScope(scope){
+            
+            scope.options = {
+                chart: {
+                    type: 'scatterChart',
+                    height: 500,
+                    color: d3.scale.category10().range(),
+                    scatter: {
+                        onlyCircles: false
+                    },
+                    showDistX: false,
+                    showDistY: false,
+                    tooltipContent: function (key) {
+                        return '<h3>' + key + '</h3>';
+                    },
+                    transitionDuration: 350,
+                    xAxis: {
+                        axisLabel: 'X Axis',
+                        tickFormat: function (d) {
+                            return d3.format('.02f')(d);
+                        }
+                    },
+                    yAxis: {
+                        axisLabel: 'Y Axis',
+                        tickFormat: function (d) {
+                            return d3.format('.02f')(d);
+                        },
+                        axisLabelDistance: 30
+                    }
+                }
+            };
+
+            scope.data = generateData(2, 40);
+
+            scope.generateDataInterval = $interval(function(){
+                scope.data = generateData(2,40);
+            }, 5000);
+
+            scope.$on('$destroy', function(){
+                if(scope.generateDataInterval != null){
+                    $interval.cancel(scope.generateDataInterval);
+                }
+            })
+            
+        }
+        
+        _initScope($scope);
+    }]);
