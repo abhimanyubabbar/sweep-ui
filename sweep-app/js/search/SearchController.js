a771219fa9a56b5b7db251a09834ffc3797b4b54
Abhimanyu Babbar <babbar@kth.se>
2015-03-16 18:34:43 +0100

    Added the ui for the search results and also linked it with the gvod service for the play functionality.

--- sweep-app/js/search/SearchController.js
+++ sweep-app/js/search/SearchController.js
@@ -6,8 +6,9 @@
 
 angular.module('app')
 
-    .controller('SearchController', ['$log', '$scope', '$routeParams', 'sweepService', function ($log, $scope, $routeParams, sweepService) {
+    .controller('SearchController', ['$log', '$scope', '$routeParams', 'sweepService','gvodService', function ($log, $scope, $routeParams, sweepService, gvodService) {
 
+        var _defaultPrefix = "http://";
 
         function _getDummyResults() {
 
@@ -15,42 +16,133 @@ angular.module('app')
                 {
                     fileName: 'Avengers: Age of Ultron',
                     uploaded: new Date(),
-                    description: 'When Tony Stark tries to jumpstart a dormant peacekeeping program, things go awry and it is up to the Avengers to stop the villainous Ultron from enacting his terrible plans.'
+                    description: 'When Tony Stark tries to jumpstart a dormant peacekeeping program, things go awry and it is up to the Avengers to stop the villainous Ultron from enacting his terrible plans.',
+                    url: 1
+
                 },
                 {
                     fileName: 'Iris',
                     uploaded: new Date(),
-                    description: 'A documentary about fashion icon Iris Apfel from legendary documentary filmmaker Albert Maysles.'
+                    description: 'A documentary about fashion icon Iris Apfel from legendary documentary filmmaker Albert Maysles.',
+                    url: 1
                 },
                 {
                     fileName: 'Cloud Atlas',
                     uploaded: new Date(),
-                    description: 'An exploration of how the actions of individual lives impact one another in the past, present and future, as one soul is shaped from a killer into a hero, and an act of kindness ripples across centuries to inspire a revolution.'
+                    description: 'An exploration of how the actions of individual lives impact one another in the past, present and future, as one soul is shaped from a killer into a hero, and an act of kindness ripples across centuries to inspire a revolution.',
+                    url: 1
                 },
                 {
                     fileName: 'Fight Club',
                     uploaded: new Date(),
-                    description: 'An insomniac office worker looking for a way to change his life crosses paths with a devil-may-care soap maker and they form an underground fight club that evolves into something much, much more...'
+                    description: 'An insomniac office worker looking for a way to change his life crosses paths with a devil-may-care soap maker and they form an underground fight club that evolves into something much, much more...',
+                    url: 1
                 },
                 {
                     fileName: 'The Dark Knight',
                     uploaded: new Date(),
-                    description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.'
+                    description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.',
+                    url: 1
                 }
 
             ]
         }
 
-        // Constructor method for the video player.
-        function _initializePlayer(obj, playerName){
 
-            obj = videojs (playerName,{}, function(){});
-            obj.dimensions ("100%", "100%");
-            obj.controls (true);
+        /**
+         * Update the video resource in the provided player
+         *
+         * @param player video player instance.
+         * @param currentResource current video resource.
+         * @param newResource updated resource.
+         * @private
+         */
+        function _updateAndPlay(player, currentResource, newResource) {
 
+            // 1. Pause the current playing
+            if (player != null) {
+                player.pause();
+            }
+
+            // 2. Inform gvod about the update
+            if (currentResource != null) {
+
+                gvodService.stop(currentResource)
+
+                    // 3. Handle the response from the gvod and based on response decide further course of action.
+                    .success(function (data) {
+
+                        $log.info(" Gvod Has successfully stopped playing the video.");
+                        _startPlaying(player, newResource);
+                    })
+
+                    .error(function (data) {
+                        $log.info(" Unable to stop the resource. ");
+                    })
+            }
+            else {
+                $log.info('Current Resource is null, so playing from start');
+                _startPlaying(player, newResource);
+            }
         }
 
 
+        /**
+         * Internal helper function for starting the video playback.
+         *
+         * @param player player
+         * @param resource video resource
+         * @private
+         */
+        function _startPlaying(player, resource) {
+
+            var name = resource['name'];
+            gvodService.play(resource)
+
+                .success(function (data) {
+
+                    $log.info("Got the port from gvod: " + data);
+                    $scope.currentVideoResource = resource;
+                    var src = _defaultPrefix.concat(gvodService.getServer().ip).concat(":").concat(data).concat('/').concat(name).concat('/').concat(name);
+
+                    $log.info("Source for the player constructed: " + src);
+                    if (player == null) {
+                        $log.warn('Player in the scope found as null. Reconstructing it .. ');
+                        player = _initializePlayer($scope.playerName);
+                    }
+
+                    player.src(src);
+                    player.load();
+                    player.play();
+                })
+
+                .error(function(data){
+                    $log.warn(" gvod play service replied with error.");
+                })
+        }
+
+
+        /**
+         * Constructor function for creating the playback resource.
+         *
+         * @param obj scope variable for player
+         * @param playerName Player Name
+         * @private
+         */
+        function _initializePlayer(playerName) {
+
+            $scope.player = videojs(playerName, {}, function () {
+            });
+            $scope.player.dimensions("100%", "100%");
+            $scope.player.controls(true);
+
+            return $scope.player;
+        }
+
+        /**
+         * Initialization of the scope.
+         * @param scope
+         */
         function initScope(scope) {
 
             scope.search = {};
@@ -63,18 +155,46 @@ angular.module('app')
             scope.search.result = _getDummyResults();
 
             // Initialize Player.
-            scope.player = videojs('main_player',{}, function(){});
-            scope.player.dimensions("100%", "100%");
-            scope.player.controls(true);
+            _initializePlayer(scope.playerName);
 
-            // Destroy Player.
+            // Destroy Player Call Back.
             scope.$on('$destroy', function () {
+
                 $log.info('Destroy the video player instance.');
-                if(scope.player != null){
+                if (scope.player != null) {
+
                     scope.player.dispose();
+                    if(scope.currentVideoResource != null){
+                        gvodService.stop($scope.currentVideoResource);
+                    }
                 }
             })
         }
+
+
+        /**
+         * Play the provided video resource.
+         * @param data
+         */
+        $scope.playResource = function (data) {
+
+            $log.info('Play Resource called with : ' + data);
+
+            var json = {
+                name: data["fileName"],
+                overlayId: parseInt(data["url"])
+            };
+
+            var test_json = {
+                name: 'demo.mp4',
+                overlayId: 12
+            };
+
+            $log.info('Reconstructing play call with : ' + test_json);
+
+            _updateAndPlay($scope.player, $scope.currentVideoResource, angular.copy(test_json));
+        };
+
 
         function _search(data) {
 
