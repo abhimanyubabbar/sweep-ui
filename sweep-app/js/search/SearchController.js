2a9a3d7c0f66ad5b4c8168f9b1e6d855173062b3
Abhimanyu Babbar <babbar@kth.se>
2015-03-16 15:05:08 +0100

    Updated the css on the search result page and added some dummy responses to check there display on Page.

--- sweep-app/js/search/SearchController.js
+++ sweep-app/js/search/SearchController.js
@@ -6,50 +6,98 @@
 
 angular.module('app')
 
-    .controller('SearchController',['$log','$scope', '$routeParams', 'sweepService', function($log, $scope, $routeParams, sweepService){
-        
-        function initScope(scope){
+    .controller('SearchController', ['$log', '$scope', '$routeParams', 'sweepService', function ($log, $scope, $routeParams, sweepService) {
 
-            scope.search={};
+
+        function _getDummyResults() {
+
+            return [
+                {
+                    fileName: 'Avengers: Age of Ultron',
+                    uploaded: new Date(),
+                    description: 'When Tony Stark tries to jumpstart a dormant peacekeeping program, things go awry and it is up to the Avengers to stop the villainous Ultron from enacting his terrible plans.'
+                },
+                {
+                    fileName: 'Iris',
+                    uploaded: new Date(),
+                    description: 'A documentary about fashion icon Iris Apfel from legendary documentary filmmaker Albert Maysles.'
+                },
+                {
+                    fileName: 'Cloud Atlas',
+                    uploaded: new Date(),
+                    description: 'An exploration of how the actions of individual lives impact one another in the past, present and future, as one soul is shaped from a killer into a hero, and an act of kindness ripples across centuries to inspire a revolution.'
+                },
+                {
+                    fileName: 'Fight Club',
+                    uploaded: new Date(),
+                    description: 'An insomniac office worker looking for a way to change his life crosses paths with a devil-may-care soap maker and they form an underground fight club that evolves into something much, much more...'
+                },
+                {
+                    fileName: 'The Dark Knight',
+                    uploaded: new Date(),
+                    description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.'
+                }
+
+            ]
+        }
+
+        // Constructor method for the video player.
+        function _initializePlayer(obj, playerName){
+
+            obj = videojs (playerName,{}, function(){});
+            obj.dimensions ("100%", "100%");
+            obj.controls (true);
+
+        }
+
+
+        function initScope(scope) {
+
+            scope.search = {};
             scope.search.searchText = $routeParams.searchText;
+            scope.playerName = 'main_player';
 
             // Initialize Resources.
-            // Initialize Video Resource.
             _search(scope.search.searchText);
 
+            scope.search.result = _getDummyResults();
 
-            scope.$on('$destroy', function(){
+            // Initialize Player.
+            scope.player = videojs('main_player',{}, function(){});
+            scope.player.dimensions("100%", "100%");
+            scope.player.controls(true);
+
+            // Destroy Player.
+            scope.$on('$destroy', function () {
                 $log.info('Destroy the video player instance.');
+                if(scope.player != null){
+                    scope.player.dispose();
+                }
             })
-
         }
 
-        function _search(data){
+        function _search(data) {
+
             $log.info("Going to perform search for : " + data);
+            //sweepService.performSearch(text)
+            //
+            //    .success(function (data) {
+            //        $log.info('Sweep Service -> Successful');
+            //        $scope.result = data;
+            //    })
+            //
+            //    .error(function (data) {
+            //        $log.info('Sweep Service -> Error');
+            //    })
         }
 
-        /**
-         * Based on the text perform search. The value should be 
-         * injected in the method, as it would help in testing.
-         */
-        
-        $scope.performSearch = function(text){
-            
-            $log.info('Perform Search Invoked');
-            
-            sweepService.performSearch(text)
-                
-                .success(function(data){
-                    $log.info('Sweep Service -> Successful');
-                    $scope.result = data;
-                })
-
-                .error(function(data){
-                    $log.info('Sweep Service -> Error');
-                })
-            
-        };
-        
-        
         initScope($scope);
+
+    }])
+
+    .directive('searchResult', ['$log', function ($log) {
+        return {
+            restrict: 'AE',
+            templateUrl: '/partials/search/search-result.html'
+        }
     }]);
